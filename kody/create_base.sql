b0f0e2286cf5935fbc83e799272ed4d10dedea9b
grudziena <56119426+grudziena@users.noreply.github.com>
2021-12-03 18:41:21 +0100

    add 'identity' & change types

--- kody/create_base.sql
+++ kody/create_base.sql
@@ -1,20 +1,20 @@
 create table Users (
-	id_user int not null primary key,
-	name_user varchar(20) not null,
-	mail varchar(10) not null,
-	password_user varchar(20) not null
+	id_user int not null primary key identity,
+	name_user nvarchar(30) not null,
+	mail nvarchar(30) not null,
+	password_user nvarchar(20) not null
 )
 create table Families (
-	id_family int not null primary key,
-	name_family varchar(10) not null
+	id_family int not null primary key identity,
+	name_family nvarchar(30) not null
 )
 create table Members (
 	id_user int not null foreign key references Users(id_user),
 	id_family int not null foreign key references Families(id_family)
 )
 create table Goals (
-	id_goal int not null primary key,
-	name_goal varchar(10) not null,
+	id_goal int not null primary key identity,
+	name_goal nvarchar(50) not null,
 	amount float not null,
 	start_date_goal date not null,
 	end_date_goal date not null,
@@ -24,8 +24,8 @@ create table Goals (
 	id_family int not null foreign key references Families(id_family)
 )
 create table Categories (
-	name_category varchar(10) not null primary key,
-	description_category varchar(60),
+	name_category nvarchar(30) not null primary key,
+	description_category varchar(100),
 	type_category int not null check(type_category in (1, -1)) --wp³ata/wyp³ata
 )
 create table Goals_payment (
@@ -35,10 +35,10 @@ create table Goals_payment (
 	id_user int not null foreign key references Users(id_user)
 )
 create table Payment (
-	id_payment int not null primary key,
+	id_payment int not null primary key identity,
 	amount float not null,
 	date_payment date not null,
 	id_user int not null foreign key references Users(id_user),
 	id_family int not null foreign key references Families(id_family),
-	category_name varchar(10) foreign key references Categories(name_category) --mo¿e byæ null, bo mo¿na ot tak zapisaæ stan konta (?)
+	category_name nvarchar(30) foreign key references Categories(name_category) --mo¿e byæ null, bo mo¿na ot tak zapisaæ stan konta (?)
 )
