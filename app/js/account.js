e080a14b4ec6c5f833097c869d3e5a6918cc61c4
grudziena <adriannagrudzien314@gmail.com>
2022-01-20 21:54:11 +0100

    updating account balance every 5 minutes

--- app/js/account.js
+++ app/js/account.js
@@ -1,7 +1,7 @@
 USERS_URL = "http://localhost:8080/api/users/"
 
 //obecny stan konta
-function update_account() {
+setTimeout(function () {
 	var url = USERS_URL + "userAccount?userId=" + USER_ID;
 	const xhr = new XMLHttpRequest();
 
@@ -12,4 +12,4 @@ function update_account() {
 		let accountBalance = xhr.response;
 		document.getElementById("accountBalance").innerHTML = accountBalance;
 	}
-}
+}, 5 * 60000); //updating every 5 minutes
