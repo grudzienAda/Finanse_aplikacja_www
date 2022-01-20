7a599376892d1afccb7d0942ced808b4d545d4e3
grudziena <adriannagrudzien314@gmail.com>
2022-01-20 22:13:31 +0100

    show account balance after page loading

--- app/js/account.js
+++ app/js/account.js
@@ -1,7 +1,9 @@
 USERS_URL = "http://localhost:8080/api/users/"
 
 //obecny stan konta
-setTimeout(function () {
+setTimeout(function () {update_account}, 5 * 60000); //updating every 5 minutes
+
+function update_account() {
 	var url = USERS_URL + "userAccount?userId=" + USER_ID;
 	const xhr = new XMLHttpRequest();
 
@@ -12,4 +14,4 @@ setTimeout(function () {
 		let accountBalance = xhr.response;
 		document.getElementById("accountBalance").innerHTML = accountBalance;
 	}
-}, 5 * 60000); //updating every 5 minutes
+}
