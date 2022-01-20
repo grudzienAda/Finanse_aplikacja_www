4898265b9ce26a25fed21c9ed751d9a85dd2a910
grudziena <adriannagrudzien314@gmail.com>
2022-01-20 20:08:48 +0100

    update

--- app/js/payment.js
+++ app/js/payment.js
@@ -1,13 +1,13 @@
 USERS_URL = "http://localhost:8080/api/payments/"
+USER_ID = sessionStorage.getItem('userID');
 
 function addPayment() {
     var amount = document.getElementById("amount").value;
     var paymentDate = document.getElementById("paymentDate").value;
     var categoryName = document.getElementById("categoryName").value;
-    var idUser;
 
 
-    var urlString = USERS_URL + "add/?amount=" + amount + "&paymentDate=" + paymentDate + "&idUser" + idUser + "&idFamily" + idFamily + "&categoryName=" + categoryName;
+    var urlString = USERS_URL + "add/?amount=" + amount + "&paymentDate=" + paymentDate + "&idUser" + USER_ID + "&idFamily" + idFamily + "&categoryName=" + categoryName;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", urlString, true);
     xhr.send();
