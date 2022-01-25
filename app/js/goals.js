b6f30f066a5a7f270d58b41cd45f9922c2404b20
grudziena <adriannagrudzien314@gmail.com>
2022-01-25 12:25:48 +0100

    add button 'add payment to goal'

--- app/js/goals.js
+++ app/js/goals.js
@@ -1,4 +1,4 @@
-USERS_URL = "http://localhost:8080/api/goals/"
+GOALS_URL = "http://localhost:8080/api/goals/"
 
 function addGoal() {
     var name = document.getElementById("goalName").value;
@@ -8,8 +8,40 @@ function addGoal() {
     var familyId;
 
 
-    var urlString = USERS_URL + "add/?name=" + name + "&goalStartDate=" + goalStartDate + "&goalEndDate=" + goalEndDate + "&userId" + userId + "&familyId" + familyId;
+    var urlString = GOALS_URL + "add/?name=" + name + "&goalStartDate=" + goalStartDate + "&goalEndDate=" + goalEndDate + "&userId" + userId + "&familyId" + familyId;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", urlString, true);
+    xhr.send();
+}
+
+//do poprawek (przekopiowane z family.js)
+function add_payment_to_goal() {
+    const email = document.getElementById("email-new-member").value;
+    const familiesList = document.getElementById("families-list");
+
+    const urlString = GOALS_URL + "add?mail=" + email + "&familyId=" + familiesList.value;
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", urlString, true);
+    xhr.send();
+}
+
+//do poprawek (przekopiowane z family.js)
+function createGoalsList(elementId) {
+    const selectList = document.getElementById(elementId);
+    const urlString = GOALS_URL + "?userId=" + localStorage.getItem("userId");
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", urlString, true);
+    xhr.responseType = "json";
+
+    xhr.onload = function() {
+        const goals = xhr.response;
+        for(let i = 0; i < goals.length; i++) {
+            const item = goals[i];
+            const option = document.createElement("option");
+            option.text = item.familyName;
+            option.value = item.familyId;
+            selectList.add(option);
+        }
+    }
     xhr.send();
 }
